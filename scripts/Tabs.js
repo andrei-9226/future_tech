7f66c4711c3abb0eae7332f61e15cbf6428d9910
andrei-9226 <andreiworkgo@gmail.com>
2025-03-08 14:13:58 +0300

    feat: add control with keys

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -37,6 +37,8 @@ class Tabs {
             const isActive = index === activeTabIndex
 
             buttonElement.classList.toggle(this.stateClasses.isActive, isActive)
+            buttonElement.setAttribute(this.stateAttributes.ariaSelected, isActive.toString())
+            buttonElement.setAttribute(this.stateAttributes.tabIndex, isActive ? '0' : '-1')
         })
 
         this.contentElements.forEach((contentElement, index) => {
@@ -51,10 +53,71 @@ class Tabs {
         this.updateUI()
     }
 
+    activateTab(newTabIndex) {
+        this.state.activeTabIndex = newTabIndex
+        this.buttonElements[newTabIndex].focus()
+    }
+
+    previousTab = () => {
+        const newTabIndex = this.state.activeTabIndex === 0 ? this.limitTabsIndex : this.state.activeTabIndex - 1
+        this.activateTab(newTabIndex)
+
+    }
+
+    nextTab = () => {
+        const newTabIndex = this.state.activeTabIndex === this.limitTabsIndex ? 0 : this.state.activeTabIndex + 1
+
+        this.activateTab(newTabIndex)
+    }
+
+    firstTab = () => {
+        this.activateTab(0)
+    }
+
+    lastTab = () => {
+        this.activateTab(this.limitTabsIndex)
+    }
+
+    onKeyDown = (event) => {
+        const { code, metaKey } = event
+
+        const action = {
+            ArrowLeft: this.previousTab,
+            ArrowRight: this.nextTab,
+            Home: this.firstTab,
+            End: this.lastTab
+        }[code]
+
+        const isMacHomeKey = metaKey && code === 'ArrowLeft'
+
+        if (isMacHomeKey) {
+            this.firstTab()
+            this.updateUI()
+            return
+        }
+
+        const isMacEndKey = metaKey && code === 'ArrowRight'
+
+        if (isMacEndKey) {
+            this.lastTab()
+            this.updateUI()
+            return
+        }
+
+        if (action) {
+            action()
+            this.updateUI()
+        }
+
+
+    }
+
     bindEvents() {
         this.buttonElements.forEach((buttonElement, index) => {
             buttonElement.addEventListener('click', () => this.onButtonClick(index))
         })
+
+        this.rootElement.addEventListener('keydown', this.onKeyDown)
     }
 
 }
